c2ea691876fa4f2fec8b1f4709cb97f60c642398
jgustavo <jgustavo0711@gmail.com>
2025-01-11 22:16:16 -0300

    commit alterado

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,6 @@
-import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
-
-import { store } from './store'
-import Router from './routes/routes'
-import Footer from './components/Footer'
-
-import GlobalStyle from './global/globalStyle'
 
 function App() {
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <GlobalStyle />
-        <Router />
-        <Footer />
-      </BrowserRouter>
-    </Provider>
   )
 }
 
